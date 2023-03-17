9da73e2f9b43d74d7fffa55ddd5abcd9463937e4
ezeigboemmanuel <ezeigboemmanuel2@gmail.com>
2023-03-17 23:31:14 +0100

    added dangerouslySetInnerHTML

--- src/App.js
+++ src/App.js
@@ -3,20 +3,19 @@ import React from "react"
 import { marked } from 'marked'
 
 function App() {
-  const [text, setText] = React.useState("");
+  const [text, setText] = React.useState("# Markdown Previewer by Ezeigbo Emmanuel \n ## FreeCodeCamp Project \n [Ezeigbo Emmanuel](https://ezeigboemmanuel.vercel.app) \n \n Here is some code, `<div></div>`, between 2 backticks. \n ```function anotherExample(firstLine,lastLine){if () {return multiLineCode;}}``` \n - And of course there are lists. \n - Some are bulleted. \n - With different indentation levels. \n - That look like this. \n \n **Text that is bold** \n \n ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)");
   function handleChange(e){
-    setText(marked.parse(e.target.value));
+    setText(e.target.value);
   }
   return (
     <div className="App">
-      <h1>MARKDOWN PREVIEWER</h1>
+      <h1>MARKDOWN EDITOR</h1>
       <div>
-        <textarea id="editor" onChange = {handleChange}>
-
+        <textarea id="editor" onChange = {handleChange} value = {text}>
         </textarea>
       </div>
       <div id = "preview">
-          {text}
+          <div dangerouslySetInnerHTML = {{ __html: marked.parse(text) }} />
       </div>
     </div>
   );
